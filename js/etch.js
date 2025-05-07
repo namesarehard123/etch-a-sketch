66c45601e88f21d2d282bd36e2d5a454fcdfbe6a
namesarehard123 <156898700+namesarehard123@users.noreply.github.com>
2025-05-07 12:47:12 +0530

    feat: add logic for grid creation

--- js/etch.js
+++ js/etch.js
@@ -1,1 +1,21 @@
-console.log("Works");
+function fillGrids(container , n) {
+    const gridWidth = container.getBoundingClientRect().width / n;
+    container.innerHtml = "";
+
+    for (let i = 0; i < n; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        for (let j = 0; j < n; j++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            grid.style.width = `${gridWidth}px`;
+            grid.style.height = `${gridWidth}px`;
+            gridRow.appendChild(grid);
+        }
+        container.appendChild(gridRow);
+    }
+
+}
+
+const container = document.querySelector(".container");
+fillGrids(container, 20);
