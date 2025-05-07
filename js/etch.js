65768df0f612d3d49bae71869fe0d1e30cffe4e5
namesarehard123 <156898700+namesarehard123@users.noreply.github.com>
2025-05-07 13:28:27 +0530

    feat: add logic for grid color filling

--- js/etch.js
+++ js/etch.js
@@ -1,6 +1,11 @@
+let color = "black";
+let gridSize = 20;
+// 0 = selected color, 1 = random, 2 = darken
+let fillType = 0;
+
 function fillGrids(container , n) {
     const gridWidth = container.getBoundingClientRect().width / n;
-    container.innerHtml = "";
+    container.innerHTML = "";
 
     for (let i = 0; i < n; i++) {
         const gridRow = document.createElement("div");
@@ -17,5 +22,41 @@ function fillGrids(container , n) {
 
 }
 
+function fillColor(element, color) {
+    element.style.backgroundColor = color;
+}
+
+function randomInt(number) {
+    return Math.floor(Math.random() * number);
+}
+
+function randomColor() {
+    return `rgb(${randomInt(256)}, ${randomInt(256)}, ${randomInt(256)})`;
+}
+
+function darken(element) {
+    if (element.style.backgroundColor !== "black") {
+        element.style.backgroundColor = "black";
+        element.style.opacity = "0.1";
+    } else {
+        element.style.opacity = `${parseFloat(element.style.opacity) + 0.1}`;
+    }
+}
+
+function handleFill(event) {
+    if (event.target.getAttribute("class") === "grid") {
+        if (event.buttons === 1) {
+            if (fillType === 0) {
+                fillColor(event.target, color);
+            } else if (fillType === 1) {
+                fillColor(event.target, randomColor());
+            } else if (fillType === 2) {
+                darken(event.target);
+            }
+        }
+    }
+}
+
 const container = document.querySelector(".container");
-fillGrids(container, 20);
+fillGrids(container, gridSize);
+container.addEventListener("mouseover", handleFill);
